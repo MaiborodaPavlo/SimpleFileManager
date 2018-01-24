4b0af9ca9db93dfd12d81dc76432902ebd185f0d
Pavel Mayboroda <Successbloger1@gmail.com>
2018-01-24 19:31:13 +0200

    Initial Commit

---
+++ SimpleFileManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleFileManager
+//
+//  Created by Pavel on 24.01.2018.
+//  Copyright © 2018 Pavel Maiboroda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleFileManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleFileManager
+//
+//  Created by Pavel on 24.01.2018.
+//  Copyright © 2018 Pavel Maiboroda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
